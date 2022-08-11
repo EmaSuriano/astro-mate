da39df9c4197b1a086c1c286f08db3603e11b024
Emanuel Suriano <emanuel.suriano@revolut.com>
2022-08-11 09:57:35 +0200

    update astro version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), image()],
+  integrations: [tailwind(), image()],
   vite: {
     ssr: {
-      external: ['svgo']
-    }
-  }
+      external: ["svgo"],
+    },
+  },
 });
